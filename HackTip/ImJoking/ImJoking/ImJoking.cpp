cdcb58f3c35f0fdfaa18ea88de92e8d91c3c8ef5
NotSurprised <roro8016489@gmail.com>
2020-03-17 19:03:06 +0800

    20200317

--- HackTip/ImJoking/ImJoking/ImJoking.cpp
+++ HackTip/ImJoking/ImJoking/ImJoking.cpp
@@ -10,8 +10,32 @@
 #define no_init_all deprecated
 #endif
 
+extern "C" __declspec(dllexport) void rundll(HWND hwnd, HINSTANCE hinst, LPTSTR lpCmdLine, INT nCmdShow) {
+
+	MessageBox(NULL, TEXT("rundll"), TEXT("Rundll32"), MB_OK);
+	system("cmd /c calc");
+	/*FILE * fp;
+	fp = fopen("Joke.txt", "w+");
+	fprintf(fp, "                                 Even Judged?\n");
+	fprintf(fp, "                                       /           Let me in?\n");
+	fprintf(fp, " No license, no problem.            ∩＿＿＿∩        / \n");
+	fprintf(fp, " 　\\     ∩＿＿＿∩                ∕︵　ㄟ |   ∩＿＿＿∩\n");
+	fprintf(fp, "　 　    |ˊ ︵ ︵ˋ               (⊙)(⊙)  ﹨ ∕︵　ㄟ  | \n");
+	fprintf(fp, "　　    ∕  (⊙)(⊙)              ≡(_●_ )   ≡(⊙)(⊙)  ﹨\n");
+	fprintf(fp, " 　　   | 　( _●_) ≡         ＿ _\\ |∪|　   /≡(_●_ )   ≡\n");
+	fprintf(fp, "     ╴≡     |∪| |          (((@         _／￣\\ |∪|     /╮\n");
+	fprintf(fp, "　 　ˋ       ﹨ˊ_ ＼          ╰╮      (((@_            Q)))\n");
+	fprintf(fp, "　　   /rundll32／  (  )           ﹨ .cpl    ﹨          |﹨\n");
+	fprintf(fp, "　　  /       ╱                    |           |   .dll  ﹨| \n");
+	fprintf(fp, " 　 　    ╴／                      \\           |          |/\n");
+	fprintf(fp, "Dll/CPL GENERATES TIME: %lu\n", (unsigned long)time(NULL));
+	fclose(fp);*/
+	return;
+}
+
 void reverse_shell()
 {
+	MessageBox(NULL, TEXT("reverse_shell"), TEXT("Rundll32"), MB_OK);
 	FILE * fp;
 	fp = fopen("Joke.txt", "w+");
 	fprintf(fp, "                                 Even Judged?\n");
@@ -34,23 +58,26 @@ void reverse_shell()
 BOOL WINAPI DllMain(HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
 {
 
-
+	
 	switch (dwReason)
 	{
+	printf("\n%d", dwReason);
 	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, TEXT("dllmain"), TEXT("Rundll32"), MB_OK);
 		reverse_shell();
+		printf("\nProcess attach of dll");
 		break;
 
 	case DLL_PROCESS_DETACH:
-
+		printf("\nProcess detach of dll");
 		break;
 
 	case DLL_THREAD_ATTACH:
-
+		printf("\nThread attach of dll");
 		break;
 
 	case DLL_THREAD_DETACH:
-
+		printf("\nThread detach of dll");
 		break;
 	}
 	return TRUE;
