8d1ae4cd1a035ad76c6eb00bef3fd52f673e905c
NotSurprised <roro8016489@gmail.com>
2020-04-06 18:32:22 +0800

    20200406

---
+++ Payload/SystemPathChk/Source/SystemPathTasks_MigratedChk/SystemPathTasks_MigratedChk/SystemPathTasks_MigratedChk.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <Lmcons.h>
+
+#pragma warning(disable : 4996)
+
+int main() {
+	char resultStr[UNLEN + 1];
+	TCHAR username[UNLEN + 1];
+	DWORD username_len = UNLEN + 1;
+	GetUserName(username, &username_len);
+	wcstombs(resultStr, username, wcslen(username));
+	sprintf(resultStr, "%S", username);
+
+	FILE * fp;
+	char fpath[2048];
+	sprintf(fpath, "C:\\Users\\%s\\Desktop\\WLtester.txt", resultStr);
+
+	fp = fopen(fpath, "a+");
+	fprintf(fp, "SystemPathTasks_MigratedBypass.\n");
+	fclose(fp);
+	return 0;
+}
