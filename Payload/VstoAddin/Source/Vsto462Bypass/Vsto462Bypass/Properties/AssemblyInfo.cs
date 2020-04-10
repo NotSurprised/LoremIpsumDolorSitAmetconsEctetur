857805f4fda0837523ebad134c3934f25f9b3c5a
NotSurprised <roro8016489@gmail.com>
2020-04-10 18:57:43 +0800

    20200410

---
+++ Payload/VstoAddin/Source/Vsto462Bypass/Vsto462Bypass/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// 組件的一般資訊是由下列的屬性集控制 
+// 變更這些屬性的值即可修改組件的相關
+// 資訊。
+[assembly: AssemblyTitle("Vsto462Bypass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vsto462Bypass")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設定為 false 會使得這個組件中的型別 
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中 
+// 的型別，請在該型別上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("7d866e4b-ffc6-4ea2-8072-dac6226f458b")]
+
+// 組件的版本資訊由下列四個值所組成: :
+//
+//      主要版本
+//      次要版本 
+//      組建編號
+//      修訂編號
+//
+// 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號 
+// 指定為預設值:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ Payload/VstoAddin/Source/Vsto462Bypass/Vsto462Bypass/ThisAddIn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace Vsto462Bypass
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filename = "/WLtester.txt";
+            path += filename;
+            using (FileStream file = File.Exists(path) ? File.Open(path, FileMode.Append) : File.Open(path, FileMode.CreateNew))
+            {
+                using (StreamWriter stream = new StreamWriter(file))
+                {
+                    stream.WriteLine("Vsto462Bypass.");
+                }
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO 產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
